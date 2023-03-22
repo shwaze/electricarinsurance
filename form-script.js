6c8bd40955b7723788c52f8cea2c59ba179f9094
shwaze <39320265+shwaze@users.noreply.github.com>
2023-03-22 18:44:43 +1100

    Update form-script.js

--- form-script.js
+++ form-script.js
@@ -19,6 +19,10 @@ window.addEventListener("load", function() {
     const insuredField = document.querySelector("input[name='insured']:checked");
     const homeownerField = document.querySelector("input[name='homeowner']:checked");
 
+    console.log("vehiclesField", vehiclesField);
+    console.log("insuredField", insuredField);
+    console.log("homeownerField", homeownerField);
+
     // Get the values of the form fields
     const zipValue = zipField.value;
     const vehiclesValue = vehiclesField ? vehiclesField.value : 0;
@@ -26,8 +30,16 @@ window.addEventListener("load", function() {
     const homeownerValue = homeownerField ? homeownerField.value : 0;
     const ageValue = ageField.value;
 
+    console.log("zipValue", zipValue);
+    console.log("vehiclesValue", vehiclesValue);
+    console.log("insuredValue", insuredValue);
+    console.log("homeownerValue", homeownerValue);
+    console.log("ageValue", ageValue);
+
     // set the query string
     const queryString = `sp=&zipcode=${zipValue}&insured=${insuredValue}&homeowner=${homeownerValue}&married=&age=${ageValue}&vehicles=${vehiclesValue}&first_name=&last_name=&email=&phone=&dply=&f=0&id=56b022dbd7797283128bbbac02fd72bd&t=1679466770&pt=&pv=YUhSMGNEb3ZMMnh2ZDJWemRDMXhkVzkwWlM1amIyMHZjMlZoY21Ob0wybHVaR1Y0TG5Cb2NEOG1ZV0k5Ykc5M1pYTjBjWFZ2ZEdWZmRtMTFiSFJwWDJOaGNnPT0%3D`;
+
+    console.log("queryString", queryString);
 
     // Update the form action
     form.action = `https://autoquotepros.com/thank-you/?${queryString}`;
