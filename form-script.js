245277c6b8b3df312694ad2189537e52bfa49397
shwaze <39320265+shwaze@users.noreply.github.com>
2023-03-22 18:59:18 +1100

    Update form-script.js

--- form-script.js
+++ form-script.js
@@ -1,16 +1,58 @@
-// Wait for the page to load
-window.addEventListener("load", function() {
+console.log("Script started");
+
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
   
   // Get references to the form and its fields
-  const form = document.querySelector("form");
+  const form = document.querySelector("#email-form");
   const zipField = document.querySelector("#zip");
   const ageField = document.querySelector("#age");
 
   // Set the default value of the age field
   ageField.value = "44";
 
+  // Check if form element is available
+  if (!form) {
+    console.error("Form not found");
+    return;
+  }
+
+  // Add event listeners for the change event on each form field
+  zipField.addEventListener("change", function() {
+    console.log("Zip code changed:", zipField.value);
+  });
+
+  ageField.addEventListener("change", function() {
+    console.log("Age changed:", ageField.value);
+  });
+
+  // Add event listeners for all radio button groups
+  const vehicleFields = document.querySelectorAll("input[name='vehicles']");
+  const insuredFields = document.querySelectorAll("input[name='insured']");
+  const homeownerFields = document.querySelectorAll("input[name='homeowner']");
+
+  vehicleFields.forEach((vehicleField) => {
+    vehicleField.addEventListener("change", function() {
+      console.log("Vehicles changed:", vehicleField.value);
+    });
+  });
+
+  insuredFields.forEach((insuredField) => {
+    insuredField.addEventListener("change", function() {
+      console.log("Insured changed:", insuredField.value);
+    });
+  });
+
+  homeownerFields.forEach((homeownerField) => {
+    homeownerField.addEventListener("change", function() {
+      console.log("Homeowner changed:", homeownerField.value);
+    });
+  });
+
   // When the form is submitted...
   form.addEventListener("submit", function(event) {
+    console.log("Form submitted");
+
     // Prevent the form from submitting normally
     event.preventDefault();
 
@@ -35,7 +77,7 @@ window.addEventListener("load", function() {
     console.log("insuredValue", insuredValue);
     console.log("homeownerValue", homeownerValue);
     console.log("ageValue", ageValue);
-
+    
     // set the query string
     const queryString = `sp=&zipcode=${zipValue}&insured=${insuredValue}&homeowner=${homeownerValue}&married=&age=${ageValue}&vehicles=${vehiclesValue}&first_name=&last_name=&email=&phone=&dply=&f=0&id=56b022dbd7797283128bbbac02fd72bd&t=1679466770&pt=&pv=YUhSMGNEb3ZMMnh2ZDJWemRDMXhkVzkwWlM1amIyMHZjMlZoY21Ob0wybHVaR1Y0TG5Cb2NEOG1ZV0k5Ykc5M1pYTjBjWFZ2ZEdWZmRtMTFiSFJwWDJOaGNnPT0%3D`;
 
